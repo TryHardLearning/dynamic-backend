b5292bf6e4c7847c3871f8a61dcae54ba79cdd0d
Lucas C. Bresolino <lucascaladob@gmail.com>
2025-08-21 22:45:36 -0300

    feat: start express

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
 import express from 'express';
+import cors from 'cors';
+
 // import { router as userRoutes } from './routes/user.routes'; // Exemplo de rota
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
-// app.use(cors()); // Se você precisar de CORS
+app.use(cors()); // Cords para permitir requisições de outros domínios
 
-// Monta as rotas
+// Routes
 // app.use('/api/users', userRoutes);
+app.get('/', (req, res) => {
+  res.send('API is running!');
+});
 
 export { app };
