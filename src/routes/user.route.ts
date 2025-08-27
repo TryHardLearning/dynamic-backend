5a4990ca2583bb1889e6a61c222bccdfb808ffb3
Lucas C. Bresolino <lucascaladob@gmail.com>
2025-08-27 04:08:29 -0300

    feat: add middleware

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { loginUser, registerUser } from '../controllers/user.controller';
+import { handleRegisterUser, handleLoginUser } from '../controllers/user.controller';
+import { validateLogin, validateRegistration } from '../middleware/user.middleware';
 
 const userRouter = express.Router();
 
-userRouter.post('/login', loginUser);
-userRouter.post('/register', registerUser);
+userRouter.post('/login', validateLogin ,handleLoginUser);
+userRouter.post('/register', validateRegistration ,handleRegisterUser);
 // userRouter.post('/admin/login', adminLogin);
 
 export default userRouter;
