852d2ce44571b4ae48005ee5a074cd323a1961cc
Lucas C. Bresolino <lucascaladob@gmail.com>
2025-08-27 05:54:26 -0300

    relase: create new route for product

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 import userRouter from './user.route';
+import productRouter from './product.route';
 
 
 const router = Router();
 
+// User route
 router.use('/users', userRouter);
+
+// Product route
+router.use('/product', productRouter);
 
 export { router };
