20384dd79851f3fc206d03cdbda11d0cdc2def5a
Lucas C. Bresolino <lucascaladob@gmail.com>
2025-08-27 11:38:34 -0300

    feat: delete, find all, find one routes

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,13 +1,20 @@
 import express from 'express';
-// import { handleCreateProduct, handleFindAllProducts, handleFindProductById, handleDeleteOneProduct } from '../controllers/product.controller';
 import upload from '../middleware/multer';
-import { handleCreateProduct } from '../controllers/product.controller';
+import { handleCreateProduct, handleDeleteOneProduct, handleFindAllProducts, handleFindProductById } from '../controllers/product.controller';
 
 const productRouter = express.Router();
 
-productRouter.post('/create', upload.fields([{name: 'image1', maxCount: 1},{name: 'image2', maxCount: 1},{name: 'image3', maxCount: 1},{name: 'image4', maxCount: 1}]),handleCreateProduct);
-// productRouter.post('/delete', handleDeleteOneProduct);
-// productRouter.post('/find/id', handleFindProductById);
-// productRouter.get('/find', handleFindAllProducts);
+productRouter.post('/create', 
+    upload.fields([
+        {name: 'image1', maxCount: 1},
+        {name: 'image2', maxCount: 1},
+        {name: 'image3', maxCount: 1},
+        {name: 'image4', maxCount: 1}
+    ]),handleCreateProduct);
+
+
+productRouter.post('/delete', handleDeleteOneProduct);
+productRouter.post('/find', handleFindProductById);
+productRouter.get('/find', handleFindAllProducts);
 
 export default productRouter;
