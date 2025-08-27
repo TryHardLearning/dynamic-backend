43f06e09363820d4b22a405dba7c6d83d7b01411
Lucas C. Bresolino <lucascaladob@gmail.com>
2025-08-27 06:36:18 -0300

    relase:  improvement in logic

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,48 +1,29 @@
 import { Request, Response } from 'express';
 import { HttpError } from '../utils/error.handle';
-import { v2 as cloudinary } from 'cloudinary';
-import ProductModel from '../models/product.model';
+import { createProduct } from '../services/product.service';
 
 // Add new product
 const handleCreateProduct = async (req: Request, res: Response) => {
     try {
         
         const { name, description, price, category, subCategory, sizes, bestSeller } = req.body;
+        console.log('Request Body:', req.body);
+
 
         const files = req.files as { [fieldname: string]: Express.Multer.File[] };
         const image1 = files.image1 && files.image1?.[0];
         const image2 = files.image2 && files.image2?.[0];
         const image3 = files.image3 && files.image3?.[0];
         const image4 = files.image4 && files.image4?.[0];
-
+       
         const images: Array<Express.Multer.File> = [image1, image2, image3, image4].filter((file): file is Express.Multer.File => file !== undefined);
-
-        let imagesUrl = await Promise.all(
-            images.map(async (file) => {
-                if (!file.path) {
-                    throw new Error('Missing required parameter - file path');
-                }
-                let result = await cloudinary.uploader.upload(file.path, { resource_type: 'image' });
-                return result.secure_url;
-            })
-        );
-
-        const productData ={
-            name,
-            description,
-            price: Number(price),
-            category,
-            subCategory,
-            sizes: JSON.parse(sizes),
-            bestSeller: bestSeller === 'true',
-            image: imagesUrl,
+        
+        if (images.length === 0) {
+            return res.status(400).json({ success: false, message: 'At least one image is required.' });
         }
 
-        // Save product to database
-
-        const product = new ProductModel(productData);
-
-        await product.save();
+        console.log(images);
+        await createProduct(name, description, price, category, subCategory, sizes, bestSeller, images);
 
         return res.status(201).json({ success: true, message: 'Product created successfully' });
     
