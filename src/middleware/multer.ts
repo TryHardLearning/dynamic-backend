53006b6e901266895ffe6f1478644e6582697f31
Lucas C. Bresolino <lucascaladob@gmail.com>
2025-08-27 05:52:59 -0300

    feat: multer for image files

---
+++ src/middleware/multer.ts
@@ -0,0 +1,11 @@
+import multer from 'multer';
+
+const storage = multer.diskStorage({
+    filename: (_, file, callback) => {
+        callback(null, file.originalname);
+    }
+});
+
+const upload = multer({ storage });
+
+export default upload;
