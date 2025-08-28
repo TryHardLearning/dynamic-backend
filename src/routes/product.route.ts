821151f40123b52dd67c37a2bc32186ac84c2ade
Lucas C. Bresolino <lucascaladob@gmail.com>
2025-08-28 08:22:03 -0300

    feat: new middleware

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -2,10 +2,11 @@ import express from 'express';
 import upload from '../middleware/multer';
 import { handleCreateProduct, handleDeleteOneProduct, handleFindAllProducts, handleFindProductById } from '../controllers/product.controller';
 import { validateCreateProduct, validateIdProduct } from '../middleware/product.middleware';
+import { adminAuthMiddleware } from '../middleware/admin.auth.middleware';
 
 const productRouter = express.Router();
 
-productRouter.post('/create', 
+productRouter.post('/create', adminAuthMiddleware, 
     upload.fields([
         {name: 'image1', maxCount: 1},
         {name: 'image2', maxCount: 1},
@@ -14,7 +15,7 @@ productRouter.post('/create',
     ]),validateCreateProduct, handleCreateProduct);
 
 
-productRouter.post('/delete', validateIdProduct, handleDeleteOneProduct);
+productRouter.post('/delete', adminAuthMiddleware, validateIdProduct, handleDeleteOneProduct);
 productRouter.post('/find', validateIdProduct, handleFindProductById);
 productRouter.get('/find', validateIdProduct, handleFindAllProducts);
 
